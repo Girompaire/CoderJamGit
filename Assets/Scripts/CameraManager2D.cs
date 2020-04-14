6e6c4772eafa729a30c56297a9aabb8ff1a1ca5e
Ophélie FOUCAULT <foucault.ophelie.pro@gmail.com>
2020-04-14 12:11:48 +0200

    Player Controller WIP and brick of LD

---
+++ Assets/Scripts/CameraManager2D.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager2D : MonoBehaviour
+{
+    public static CameraManager2D instance;
+
+    public enum FollowMode
+    {
+        SPEED,
+        RATIO
+    }
+    [Header ("Follow Settings")]
+    public FollowMode followMode = FollowMode.SPEED;
+    public float followSpeed = 5f;
+    [Range(0f,1f)]
+    public float followRatio = 0.5f;
+    public Transform followTarget;
+
+    [System.Serializable]
+
+    public class CameraBounds
+    {
+        public float Top =1f;
+        public float Left=-1f;
+        public float Right=1f;
+        public float Bottom=-1f;
+    }
+
+    [Header("Bounds Settings")]
+    public CameraBounds _cameraBounds;
+
+    private Camera _camera;
+
+    private void Awake()
+    {
+        instance = this;
+        _camera = GetComponentInChildren<Camera>();
+    }
+    private void LateUpdate()
+    {
+        Vector3 position = transform.position;
+        Vector3 followPosition = followTarget.position;
+        followPosition = _ClampPositionToScreen(followTarget.position);
+        switch (followMode)
+        {
+            case FollowMode.SPEED:
+                position.x = Mathf.Lerp(position.x, followPosition.x, Time.deltaTime * followSpeed);
+                position.y = Mathf.Lerp(position.y, followPosition.y, Time.deltaTime * followSpeed);
+                break;
+            case FollowMode.RATIO:
+                position.x = Mathf.Lerp(position.x, followPosition.x, followRatio * Time.timeScale);
+                position.y = Mathf.Lerp(position.y, followPosition.y, followRatio * Time.timeScale);
+                break;
+
+        }
+        transform.position = position;
+    }
+
+    private Vector3 _ClampPositionToScreen(Vector3 position)
+    {
+        Vector3 bottomLeft = _camera.ScreenToWorldPoint(Vector3.zero);
+        Vector3 topRight = _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth,_camera.pixelHeight,0f));
+
+        Vector2 screenSize = new Vector2(topRight.x - bottomLeft.x, topRight.y - bottomLeft.y);
+        Vector2 halfScreenSize = screenSize / 2f;
+
+        position.x = Mathf.Clamp(position.x, _cameraBounds.Left + halfScreenSize.x,_cameraBounds.Right - halfScreenSize.x);
+        position.y = Mathf.Clamp(position.y, _cameraBounds.Bottom + halfScreenSize.y,_cameraBounds.Top - halfScreenSize.y);
+
+        return position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        float gizmosCubeSize = 0.3f;
+
+        Vector3 topLeft = new Vector3(_cameraBounds.Left, _cameraBounds.Top, transform.position.z);
+        Gizmos.DrawCube(topLeft, Vector3.one * gizmosCubeSize);
+
+        Vector3 bottomLeft = new Vector3(_cameraBounds.Left, _cameraBounds.Bottom, transform.position.z);
+        Gizmos.DrawCube(bottomLeft, Vector3.one * gizmosCubeSize);
+
+        Vector3 topRight = new Vector3(_cameraBounds.Right, _cameraBounds.Top, transform.position.z);
+        Gizmos.DrawCube(topRight, Vector3.one * gizmosCubeSize);
+
+        Vector3 bottomRight = new Vector3(_cameraBounds.Right, _cameraBounds.Bottom, transform.position.z); ;
+        Gizmos.DrawCube(bottomRight, Vector3.one * gizmosCubeSize);
+
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, bottomLeft);
+        Gizmos.DrawLine(bottomLeft, topLeft);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Vector2 _velocity;
+
+    public float speed = 5f;
+
+    [Range(1, 100)]
+    public float jumpVelocity;
+    public float myGravity;
+    private Vector2 velocityStock;
+    private bool isGravity = true;
+
+    private bool isJumping;
+    public float countJump = 5.0f;
+    private float initialCount;
+
+
+    private bool isGrounded;
+    private List<GameObject> GroundColliders;
+
+    public GameObject roue;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        initialCount = countJump;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGravity)
+        {
+            float moveDirX = Input.GetAxis("Horizontal");
+            Vector2 moveDir = new Vector2(moveDirX, 0);
+
+            _rigidbody.velocity = new Vector2(moveDir.normalized.x * speed, _rigidbody.velocity.y);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isGrounded)
+                {
+                    Jump();
+                }
+            }
+
+            if (isJumping)
+            {
+                countJump -= Time.deltaTime;
+                if (countJump < 0)
+                {
+                    isJumping = false;
+                    countJump = initialCount;
+                }
+            }
+            else
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y - (myGravity * Time.deltaTime));
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            Choose();
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha0))
+        {
+            Release();
+        }
+    }
+
+    private void Jump() // Jump de Van Halen
+    {
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpVelocity);
+        isJumping = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            GroundColliders.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            GroundColliders.Remove(collision.gameObject);
+            if (GroundColliders.Count == 0)
+            {
+                isGrounded = false;
+            }
+        }
+    }
+
+    private void Choose() //Choose de Why Don't We
+    {
+        velocityStock = _rigidbody.velocity;
+        _rigidbody.velocity *=0;
+        isGravity = false;
+        roue.SetActive(true);
+    }
+
+    private void Release()
+    {
+        _rigidbody.velocity = velocityStock;
+        isGravity = true;
+        roue.SetActive(false);
+    }
+
+
+}
